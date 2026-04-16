89ea1b23a8b3303b99f9f908b61edab3c177d84d
lnx645 <dadanhidyt@gmail.com>
2026-04-16 11:40:49 +0700

    update

--- routes/creator.php
+++ routes/creator.php
@@ -3,6 +3,3 @@
 use App\Http\Controllers\IndexPageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/creator/account/login",App\Http\Controllers\Creator\LoginController::class)->name("loginCreatorView");
-Route::post("/creator/account/login",[App\Http\Controllers\Creator\LoginController::class,'checkLogin'])->name("loginCreatorAction");
-
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get("/{role}/account/login",App\Http\Controllers\Creator\LoginController::class)->name("loginCreatorView");
+Route::post("/{role}/account/login",[App\Http\Controllers\Creator\LoginController::class,'checkLogin'])->name("loginCreatorAction");
+
+
 include base_path("routes/app.php");
 include base_path("routes/creator.php");
